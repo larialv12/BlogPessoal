8fe8020abb534179b4214fb22c407f0f72484f1f
LarissaSiqueira <larialves200157@gmail.com>
2025-03-09 19:23:38 -0300

    parte 2

--- src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -1,6 +1,3 @@
-
-
-
 package com.generation.blogpessoal.model;
 
 import java.time.LocalDateTime;
@@ -18,22 +15,22 @@ import jakarta.validation.constraints.Size;
 @Entity
 @Table(name = "tb_postagens")
 public class Postagem {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @NotBlank(message = "O atributo título é Obrigatório!")
-    @Size(min = 5, max = 100, message = "O atributo título deve conter no mínimo 05 e no máximo 100 caracteres")
-    private String titulo;
-
-    @NotBlank(message = "O atributo texto é Obrigatório!")
-    @Size(min = 10, max = 1000, message = "O atributo texto deve conter no mínimo 10 e no máximo 1000 caracteres")
-    private String texto;
-
-    @UpdateTimestamp
-    private LocalDateTime data;
     
+    @Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) 
+	private Long id;
+	
+	@NotBlank(message = "O atributo título é Obrigatório!") 
+	@Size(min = 5, max = 100, message = "O atributo título deve conter no mínimo 05 e no máximo 100 caracteres")
+	private String titulo;
+	
+	@NotBlank(message = "O atributo texto é Obrigatório!")
+	@Size(min = 10, max = 1000, message = "O atributo texto deve conter no mínimo 10 e no máximo 1000 caracteres")
+	private String texto;
+	
+	@UpdateTimestamp
+	private LocalDateTime data;
+
     public Long getId() {
         return this.id;
     }
@@ -65,6 +62,5 @@ public class Postagem {
     public void setData(LocalDateTime data) {
         this.data = data;
     }
-
+  
 }
-
